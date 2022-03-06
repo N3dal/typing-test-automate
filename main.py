2b1ea6cec7b1d8df8f59ca740cc69c1b13dc6d6b
Ned-84 <nedalxzo@gmail.com>
2022-03-06 16:47:21 +0200

    creat the basic structure for the script.

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 # simple script for automate typing tests,
 # in test sites.
 # cheating in sites like typing racer.
-#
+# note the website that i use for this is:
+# "https://www.typingpal.com/en/typing-test"
 #
 #
 # Author:N84.
@@ -13,3 +14,30 @@
 # ///
 # ///
 # -----------------------------------------------------------------
+
+
+from os import system
+from os import name as os_name
+
+
+def clear():
+    """wipe the terminal screen"""
+
+    if os_name == "posix":
+        # *nix machines.
+        system("clear")
+
+    else:
+        # windows machines.
+        system("cls")
+
+
+clear()
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
