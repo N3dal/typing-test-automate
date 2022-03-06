a5bd261aef03b1b0b436f4ca016d3a7e4921ab13
Ned-84 <nedalxzo@gmail.com>
2022-03-06 16:41:35 +0200

    create the main script

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------------
+# simple script for automate typing tests,
+# in test sites.
+# cheating in sites like typing racer.
+#
+#
+#
+# Author:N84.
+#
+# Create Date:Sun Mar  6 16:36:53 2022.
+# ///
+# ///
+# ///
+# -----------------------------------------------------------------
