c2aae5b3c4c0448d7eb4f1052847ea5af989fce0
Ned-84 <nedalxzo@gmail.com>
2022-03-06 16:57:09 +0200

    create typing function

--- main.py
+++ main.py
@@ -37,10 +37,24 @@ def clear():
 clear()
 
 
-def main():
+def typing(msg: str, period: int = 250e-3):
+    """this function will type the msg using,
+    the physical keyboard."""
 
     # create the main Keyboard controller object.
     keyboard = Controller()
+
+    for char in msg:
+        keyboard.press(char)
+        sleep(period)
+
+
+def main():
+
+    sleep(15)
+
+    msg = "To learn to type quickly, practice often and adopt the proper technique. Fix your posture, have adequate lighting, position your hands correctly over the keyboard, look at the screen and use all your fingers to hit the keys. At first, concentrate on accuracy over speed. This will help you develop muscle memory and create automatic reflexes. Keep practicing and gradually pick up the pace. You'll see results after just a few weeks! To learn to type quickly, practice often and adopt the proper technique. Fix your posture, have adequate lighting, position your hands correctly over the keyboard, look at the screen and use all your fingers to hit the keys. At first, concentrate on accuracy over speed. This will help you develop muscle memory and create automatic reflexes. Keep practicing and gradually pick up the pace. You'll see results after just a few weeks!"
+    typing(msg, period=50e-3)
 
 
 if __name__ == "__main__":
